fc31d0b39672a5eb1a0d7b35f3840ac562b75e21
TRClassic <82931913+trclassic92@users.noreply.github.com>
2024-12-20 17:21:28 -0600

    To The Moon

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'TRClassic'
+
+files {
+  'mp_m_freemode_01_tr_spin_hat.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_tr_spin_hat.meta'
